42b506f1ce2e3ad879eab5701655a36ae41ba8b4
TusharST360 <139489960+TusharST360@users.noreply.github.com>
2023-07-21 12:16:33 +0530

    Create EXPERIMENT1.cpp

---
+++ EXPERIMENT1.cpp
@@ -0,0 +1,25 @@
+/*  PRN:22070123125
+    Experiment 1
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b;
+    cout<<"Hello World!";
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"The sum of a and b is: " << a+b <<endl;
+    cout<<"The difference of a and b is: " << a-b<<endl;
+    cout<<"The product of a and b is: " << a*b<<endl;
+    cout<<"The division of a and b is: " << a/b<<endl;
+    return 0;
+}
+
+/*  OUTPUT
+Hello World!
+Enter two numbers: 3 2
+The sum of a and b is: 5
+The difference of a and b is: 1
+The product of a and b is: 6
+The division of a and b is: 1.5
+*/
